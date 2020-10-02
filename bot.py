5537dc70c5ef3717b58dcfd1efba492f842ee14e
Jonathan <30177086+MonliH@users.noreply.github.com>
2020-10-02 01:32:26 +0000

    Various improvements (#1)
    * Add channel_ids.json to gitignore
    
    * Make code more idiomatic
    
    * Make commands case insensitive
    
    * Better error handling
    
    * Reset channels

--- bot.py
+++ bot.py
@@ -8,17 +8,22 @@ url_regex = re.compile(
     r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)"
 )
 
-bot = commands.Bot(command_prefix="$")
+bot = commands.Bot(command_prefix="$", case_insensitive=True)
 
 
-def set_stuff(ctx, channel_name, channel_id: discord.TextChannel):
+class MediaType:
+    Link = "links"
+    Photo = "photos"
+
+
+def set_channel(ctx, channel_type: MediaType, channel_id: discord.TextChannel):
     server_id = str(ctx.message.guild.id)
     with open("channel_ids.json", "r") as channel_file:
         channel_ids = json.load(channel_file)
     if server_id in channel_ids:
-        channel_ids[server_id][channel_name] = channel_id.id
+        channel_ids[server_id][str(channel_type)] = channel_id.id
     else:
-        channel_ids[server_id] = {channel_name: channel_id.id}
+        channel_ids[server_id] = {str(channel_type): channel_id.id}
     with open("channel_ids.json", "w") as channel_file:
         json.dump(channel_ids, channel_file)
 
@@ -29,7 +34,7 @@ async def set_link(ctx, channel_id: discord.TextChannel):
     if not ctx.message.author.guild_permissions.administrator:
         await ctx.send(f"🚧 Admins only 🚧")
         return
-    set_stuff(ctx, "links", channel_id)
+    set_channel(ctx, MediaType.Link, channel_id)
     await ctx.send(
         f"{ctx.author.mention} The links channel has been set to <#{channel_id.id}>"
     )
@@ -41,12 +46,13 @@ async def set_photo(ctx, channel_id: discord.TextChannel):
     if not ctx.message.author.guild_permissions.administrator:
         await ctx.send(f"🚧 Admins only 🚧")
         return
-    set_stuff(ctx, "photos", channel_id)
+    set_channel(ctx, MediaType.Photo, channel_id)
     await ctx.send(
         f"{ctx.author.mention} The photos channel has been set to <#{channel_id.id}>"
     )
 
 
+@bot.listen("on_ready")
 async def on_ready():
     print("Logged on as {0}!".format(bot.user))
     await bot.change_presence(
@@ -54,6 +60,18 @@ async def on_ready():
     )
 
 
+@bot.event
+async def on_command_error(ctx, error):
+    await ctx.send(f"Error: `{error}`")
+
+
+async def get_media(media_type: MediaType, message, channel_ids, guild_id):
+    if media_type in channel_ids[guild_id]:
+        channel = bot.get_channel(channel_ids[guild_id][media_type])
+        await channel.send(message)
+
+
+@bot.listen("on_message")
 async def on_message(message):
     if message.author.id == bot.user.id:
         return
@@ -64,21 +82,12 @@ async def on_message(message):
         return
     for attachment in message.attachments:
         if img_regex.match(attachment.url):
-            if "photos" in channel_ids[guild_id]:
-                channel = bot.get_channel(channel_ids[guild_id]["photos"])
-                await channel.send(attachment.url)
+            await get_media(MediaType.Photo, attachment.url, channel_ids, guild_id)
     if img_regex.match(message.content):
-        if "photos" in channel_ids[guild_id]:
-            channel = bot.get_channel(channel_ids[guild_id]["photos"])
-            await channel.send(message.content)
+        await get_media(MediaType.Photo, message.content, channel_ids, guild_id)
     elif url_regex.match(message.content):
-        if "links" in channel_ids[guild_id]:
-            channel = bot.get_channel(channel_ids[guild_id]["links"])
-            await channel.send(message.content)
+        await get_media(MediaType.Link, message.content, channel_ids, guild_id)
 
-
-bot.add_listener(on_ready)
-bot.add_listener(on_message, "on_message")
 
 with open("token.txt", "r") as f:
     token = f.read()
